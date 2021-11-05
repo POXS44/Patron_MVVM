05f1dbe6c2744d1cd7ec561f0c67282734b1806f
POXS44 <vzeledon7@gmail.com>
2021-11-05 10:38:18 -0600

    Modelo MVVM TERMINADO MAIS O MENOS

--- app/src/main/java/com/poxs44/mvvm/AlCuadradoActivity.java
+++ app/src/main/java/com/poxs44/mvvm/AlCuadradoActivity.java
@@ -1,8 +1,11 @@
 package com.poxs44.mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ public class AlCuadradoActivity extends AppCompatActivity {
     private EditText etAlcuadrado;
     private Button btAlcuadrado;
     private TextView tvAlcuadrado;
+    private AlCuadradoViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +25,25 @@ public class AlCuadradoActivity extends AppCompatActivity {
     }
 
     private void configView(){
+        viewModel= new ViewModelProvider(this).get(AlCuadradoViewModel.class);
         tvAlcuadrado = findViewById(R.id.tvAlcuardado);
         etAlcuadrado = findViewById(R.id.edAlcuadrado);
         btAlcuadrado = findViewById(R.id.btCalcular);
 
+
+        btAlcuadrado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.alCuadrado(etAlcuadrado.getText().toString());
+            }
+        });
+
+        final Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onChanged(String resultado) {
+                tvAlcuadrado.setText(resultado);
+            }
+        };
+        viewModel.getResultado().observe(this,observer);
     }
 }
--- app/src/main/java/com/poxs44/mvvm/AlCuadradoUseCase.java
+++ app/src/main/java/com/poxs44/mvvm/AlCuadradoUseCase.java
@@ -2,6 +2,10 @@ package com.poxs44.mvvm;
 
 public class AlCuadradoUseCase {
     public static String alCuadrado(String data){
+        if (data.equals(""))
+        {
+            return "Campo Vacio";
+        }
         return String.valueOf(Double.valueOf(data)*Double.valueOf(data));
 
     }
