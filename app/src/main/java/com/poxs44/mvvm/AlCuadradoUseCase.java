2552b47f2851c7c62627ec40fef63312062e7844
POXS44 <vzeledon7@gmail.com>
2021-11-05 10:15:41 -0600

    Ya estan listos los componentes del ViewModel

---
+++ app/src/main/java/com/poxs44/mvvm/AlCuadradoUseCase.java
@@ -0,0 +1,9 @@
+package com.poxs44.mvvm;
+
+public class AlCuadradoUseCase {
+    public static String alCuadrado(String data){
+        return String.valueOf(Double.valueOf(data)*Double.valueOf(data));
+
+    }
+
+}
