f721f0be311a2fec6e2004a0dcb20d49a30dbcc1
POXS44 <vzeledon7@gmail.com>
2021-11-05 09:50:40 -0600

    Primero declaramos las variables

--- app/src/main/java/com/poxs44/mvvm/AlCuadradoActivity.java
+++ app/src/main/java/com/poxs44/mvvm/AlCuadradoActivity.java
@@ -3,9 +3,15 @@ package com.poxs44.mvvm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AlCuadradoActivity extends AppCompatActivity {
 
+    private EditText etAlcuadrado;
+    private Button btAlcuadrado;
+    private TextView tvAlcuadrado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
